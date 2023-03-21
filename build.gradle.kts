b43984f6adef3d35fe33b521b9aa4da14515aa32
Miguel Angel Lopez Romero <michayita@gmail.com>
2023-03-21 22:05:12 +0100

    Update Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("script-runtime"))
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation 'com.squareup.retrofit2:converter-gson:2.9.0'
-    implementation 'com.google.code.gson:gson:2.8.8'
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.8.8")
 }
 
 tasks.register("runScript") {
@@ -33,9 +33,5 @@ repositories {
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     jvmTarget = "1.8"
-}
 
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "retos-programacion-2023"
+include(":retos-programacion-2023")
+project(":retos-programacion-2023").projectDir = rootProject.file("retos-programacion-2023")
